1c0fb3335175fd817456c3c80a5056591d341b76
Aleksei Elokhin <adwiza@gmail.com>
2025-03-02 14:21:30 +0300

    chore(other): Change istioinit

--- Istio-init.sh
+++ Istio-init.sh
@@ -3,5 +3,4 @@ helm repo update
 helm install istio-base istio/base -n istio-system --set defaultRevision=default --create-namespace
 helm install istiod istio/istiod -n istio-system --wait
 kubectl create namespace istio-ingress
-helm install istio-ingress istio/gateway -n istio-ingress --wait
 helm install istio-ingressgateway istio/gateway -n istio-ingress
