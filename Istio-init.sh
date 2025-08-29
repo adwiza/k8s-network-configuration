00e3469c3f8c131271c3a83d79dba74150860227
Aleksei Elokhin <adwiza@gmail.com>
2025-08-29 10:07:41 +0300

    feat(other) Add some changes to istio stuff

--- Istio-init.sh
+++ Istio-init.sh
@@ -2,5 +2,4 @@ helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo update
 helm upgrade --install istio-base istio/base -n istio-system --set defaultRevision=default --create-namespace
 helm upgrade --install istiod istio/istiod -n istio-system --wait
-kubectl create namespace istio-ingress
-helm upgrade --install istio-ingressgateway istio/gateway -n istio-ingress
+helm upgrade --install istio-ingressgateway istio/gateway -n istio-ingress --create-namespace
